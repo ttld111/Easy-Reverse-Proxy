ecf76a2fbf481a9ef6cd01535caee50e0c38dd60
ttld111 <2971239586@qq.com>
2025-04-12 22:14:57 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -2,6 +2,12 @@ import type {Proxy} from './util/model';
 
 export let PORT = 377; // 端口
 
-export let PROXY_CODE_SECRET = 'Easy-Reverse-Proxy'; // 代理码密钥
+export let PROXY_CODE_SECRET = 'titiliandu'; // 代理码密钥
 // 代理
-export let PROXIES: Proxy[] = [];
+export let PROXIES: Proxy[] = [
+    {
+        domain: "rts.ruby-jellyfin.top",
+        url: "https://www.reuters.com/",
+        enable: true
+    }
+];
